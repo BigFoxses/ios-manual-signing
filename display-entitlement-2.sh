dc4c1494cd36d8218c71f459889c33ae61e4127e
yipman <redbulls123@gmail.com>
2020-06-21 21:23:55 +0800

    update script
    	modified:   display-entitlement-2.sh
    	modified:   display-entitlement.sh

--- display-entitlement-2.sh
+++ display-entitlement-2.sh
@@ -1,11 +1,14 @@
-#!bin/bash
-DIR=$PWD
-if ! [ "$1" == ""  ] ; then
-    DIR="$1"
-fi
+#!/bin/bash
+echo "THIS SCRIPT IS USED TO DISPLAY ENTITLEMENT OF An IPA"
+if [ "$1" == "" ]; then
+    echo "Usage ent2 <xx.ipa>"
+    exit -1
+fi 
+TMP_DIR=$(mktemp -d)
+unzip $1 -d $TMP_DIR &> /dev/null
 IFS='
 '
-for i in `find $DIR  -exec file '{}' \; | grep -i mach | cut -d":" -f 1 | cut -d"(" -f 1 | awk '{$1=$1;print}'|  sort -u -k1`; do
+for i in `find $TMP_DIR  -exec file '{}' \; | grep -i mach | cut -d":" -f 1 | cut -d"(" -f 1 | awk '{$1=$1;print}'|  sort -u -k1`; do
     echo "************************"
     echo $i
     codesign -d --ent :- $i
--- display-entitlement.sh
+++ display-entitlement.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-DIR=$PWD
-if ! [ "$1" == "" ] ; then
-    DIR="$1"
-fi
+echo "THIS SCRIPT IS USED TO DISPLAY ENTITLEMENT OF APPDOME PRESIGNED IPA"
+if [ "$1" == "" ]; then
+    echo "Usage ent <xx.ipa>"
+    exit -1
+fi 
+TMP_DIR=$(mktemp -d)
+unzip $1 -d $TMP_DIR &> /dev/null
 IFS='
 '
-for i in `find $DIR -name entitlement\*.plist` ; do
+for i in `find $TMP_DIR -name entitlement\*.plist` ; do
     echo "*********************"
     echo $i
     cat $i
 done
+rm -rf $TMP_DIR
